14bf29998ae981ce3e395e7e63d48661d34dc130
kimyuri <dsuis1310009@naver.com>
2023-01-31 12:26:10 +0900

    yuri

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) { // Member타입으로 받아와서
+        member.setId(++sequence); // 고객아이디는 시스템상 입력
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // 받아온 이름(name)과 멤버안의 이름이 같다면 그 결과 반환
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
