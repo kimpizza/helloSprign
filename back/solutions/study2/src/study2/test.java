000e66fab9fad5622e3bc8cc161b08f7c1d8d2a9
kimyuri <dsuis1310009@naver.com>
2023-03-13 15:17:11 +0900

    bfs

--- back/solutions/study2/src/study2/test.java
+++ back/solutions/study2/src/study2/test.java
@@ -8,70 +8,39 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class test {
-	static int N, M;
-	static int [][]arr;
-	static boolean[][] visit;
-	
-	static int[] moveX = {-1, 1, 0, 0}; //상 하 좌 우 
-	static int[] moveY = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[]args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		
-		arr = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			char[] c = str.toCharArray();
-			for(int j = 0; j<c.length; j++) {
-				arr[i][j] = Character.getNumericValue(c[j]);
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 0; t<T; t++) {
+			int num = Integer.parseInt(br.readLine());
+			int [] arr = new int[num+1];
+			boolean [] visit = new boolean[num+1];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 1; i<arr.length; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-		}
-		visit = new boolean[N][M];
-		visit[0][0] = true;
-		bfs(0,0);
-		
-		System.out.println(arr[N-1][M-1]);
-		
-	}
-	public static void bfs(int x, int y) {
-		Queue<t> q = new LinkedList<>();
-		q.add(new t(x,y));
-		
-		while(!q.isEmpty()) {
-			t s = q.poll(); // 0,0 꺼내오기 
-			for(int i = 0; i<4; i++) {
-				int nextX = s.x + moveX[i];
-				int nextY = s.y + moveY[i];
-				
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY>=M) {
-					continue;
+			// 값 세팅 완료
+			// 1 2 3 4 5 6 7 8 
+			// 3 2 7 8 1 4 5 6 
+			int cnt = 0;
+			for(int i = 1; i<arr.length; i++) {
+				if(!visit[i]) {
+					visit[i] = true;
+					int go = arr[i];
+					while(true) {
+						if(visit[go]) {
+							visit[go]=true;
+							cnt++;
+							break;
+						} //방문한적이 없다면 
+						visit[go] = true;
+						go = arr[go];
+					}
 				}
-				if(visit[nextX][nextY] || arr[nextX][nextY]==0) {
-					continue;
-				}
-				
-				q.add(new t(nextX, nextY));
-				arr[nextX][nextY] = arr[s.x][s.y] + 1;
-				visit[nextX][nextY] = true;
-				
-				
 			}
-		}
-	}
-
-}
-
-class t{
-	int x;
-	int y;
-	t(int x, int y){
-		this.x = x;
-		this.y = y;
+			System.out.println(cnt);
+		}br.close();
 	}
 }
