3507c2405535a3eaeb35bb119d3846fcf9365115
kimyuri <dsuis1310009@naver.com>
2023-03-20 21:58:54 +0900

    greedy

--- back/solutions/study0319/src/study0319/bj1931.java
+++ back/solutions/study0319/src/study0319/bj1931.java
@@ -10,38 +10,54 @@ public class bj1931 {
 	 * 4. 리스트 사용해볼까???
 	 * */
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int [][]arr = new int [n][2];
-		for(int i = 0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			arr[i][0] = a;
-			arr[i][1] = b;
-		}
-		Arrays.sort(arr, new Comparator<int[]>() {
-			//새로운 정렬기준 세우기 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1] == o2[1]) {
-					//종료 시간이 같다면 ?
-					return o1[0] - o2[0];
-				}
-				return o1[1]-o1[1];
-			}
-		});
-		int cnt = 0; 
-		int prev = 0;
 		
-		for(int i = 0 ; i<n; i++) {
-			if(prev <=arr[i][0]) { //새로 들어오는 시간의 시작 시간이 이전의 종료시간보다 더 크면
-				prev = arr[i][1];
-				System.out.println(arr[i][0]+ " " + arr[i][1]);
-				cnt++;
+				Scanner in = new Scanner(System.in);
+				
+				int N = in.nextInt();
+				
+				
+				/*
+				  time[][0] 은 시작시점을 의미 
+				  time[][1] 은 종료시점을 의미 
+				*/
+				int[][] time = new int[N][2];
+				
+				
+				for(int i = 0; i < N; i++) {
+					time[i][0] = in.nextInt();	// 시작시간 
+					time[i][1] = in.nextInt();	// 종료시간 
+				}
+				
+				
+				// 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의 
+				Arrays.sort(time, new Comparator<int[]>() {
+					
+					@Override
+					public int compare(int[] o1, int[] o2) {
+						
+						// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.  
+						if(o1[1] == o2[1]) {
+							return o1[0] - o2[0];
+						}
+						
+						return o1[1] - o2[1];
+					}
+		 
+				});
+				
+				int count = 0;
+				int prev_end_time = 0;
+				
+				for(int i = 0; i < N; i++) {
+					
+					// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
+					if(prev_end_time <= time[i][0]) {
+						prev_end_time = time[i][1];
+						count++;
+					}
+				}
+				
+				System.out.println(count);
 			}
+		 
 		}
-		System.out.println(cnt);
-	}
-}
